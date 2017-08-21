337075fafb4d1bf1f3922c0f40c97ed905522cee
Javen <w@juyii.com>
2017-08-21 16:32:28 +0800

    Initialize

---
+++ src/SayHello.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hayashikoubun
+ * Date: 2017/8/18
+ * Time: 下午5:40
+ */
+namespace Sunong\StockControl;
+
+class SayHello{
+    static public function say(){
+        echo 'hallo world';
+    }
+}
