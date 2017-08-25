eb1efd40196b2c240f7bc7d3bdd48d957b966679
Javen <w@juyii.com>
2017-08-25 16:00:57 +0800

    销售扣减库存方法新增

--- src/function/StockFunc.php
+++ src/function/StockFunc.php
@@ -8,6 +8,7 @@ namespace SuNong\StockControl\Func;
 
 use Illuminate\Support\Facades\DB;
 use SuNong\StockControl\Model\Stock;
+use SuNong\StockControl\Model\StockBatch;
 
 class StockFunc extends CommonFunc{
 
@@ -90,6 +91,45 @@ class StockFunc extends CommonFunc{
             $this->log_record('error','null','库存信息初始化失败'.' 耗时:'.($this->get_micro_time()-$start_time).' 原因:'.json_encode($exception->getMessage()),$params);
             return $result;
         }
+
+    }
+
+    /**
+     * @author Javen <w@juyii.com>
+     * @date 2017-08-25
+     * @param string $hq_code
+     * @param integer $orgz_id 组织id
+     * @param integer $related_id 相关单据id
+     * @param integer $genre 单据类型
+     * @param array $products 商品信息
+     * @return array|mixed 销售扣减库存
+     */
+    public function sales_deduct_stock($hq_code,$orgz_id,$related_id,$genre,$products){
+        $start_time=$this->get_micro_time();
+        $params=func_get_args();
+        $this->log_record('info','null','销售扣减库存开始',$params);
+        $hq_code=trim($hq_code)!=''?$hq_code:null;
+        $orgz_id=is_numeric($orgz_id)?$orgz_id:null;
+        $related_id=is_numeric($related_id)?$related_id:null;
+        $genre=is_numeric($genre)?$genre:null;
+        $products=is_array($products) && !empty($products) ? $products : null;
+
+        if(!$hq_code || !$orgz_id || !$products || !$related_id || !$genre){
+            $result=['code'=>'10000','msg'=>'参数缺失'];
+            $this->log_record('error','null','销售扣减库存失败:参数缺失',$params);
+            return $result;
+        }
+
+        $stock_batch_model=new StockBatch();
+        $result=$stock_batch_model->deduct_stock_batch($hq_code,$orgz_id,$related_id,$genre,$products,3);
+        if(!$result){
+            $result=['code'=>'10000','msg'=>'销售扣减库存失败:库存变动失败或库存信息不存在'];
+            $this->log_record('error','null','销售扣减库存失败:库存变动失败或库存信息不存在',$params);
+            return $result;
+        }
+        $result=['code'=>'0','msg'=>'销售扣减库存成功','amount'=>$result['amount'],'data'=>$result['detail']];
+        $this->log_record('error','null','销售扣减库存成功 耗时:'.($this->get_micro_time()-$start_time),$params);
+        return $result;
 
     }
 }
